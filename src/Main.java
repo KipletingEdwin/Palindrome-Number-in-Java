3b4350c8648d0569d9e924d997a0e2776a111103
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-15 08:40:56 +0300

    Add try and catch error

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         int number;
         System.out.println("Enter the number : ");
         Scanner scanner = new Scanner(System.in);
-        number = scanner.nextInt();
-        int reversedNumber = 0, temp = number;
+        try {
+            number = scanner.nextInt();
+            int reversedNumber = 0, temp = number;
 
-        while (temp > 0){
-            int remainder = temp % 10;
-            reversedNumber = reversedNumber * 10 + remainder;
-            temp = temp /10;
+            while (temp > 0) {
+                int remainder = temp % 10;
+                reversedNumber = reversedNumber * 10 + remainder;
+                temp = temp / 10;
+            }
+            if (number == reversedNumber)
+                System.out.println("The number is Palindrome");
+            else
+                System.out.println("The number is not Palindrome");
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid integer.");
         }
-        if (number == reversedNumber)
-            System.out.println("The number is Palindrome");
-        else
-            System.out.println("The number is not Palindrome");
     }
 }
