412ad44408ca1af62bbb0b26ff8a98677b8d7a7d
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-15 08:33:46 +0300

    finalize the palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int number;
+        System.out.println("Enter the number : ");
+        Scanner scanner = new Scanner(System.in);
+        number = scanner.nextInt();
+        int reversedNumber = 0, temp = number;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (temp > 0){
+            int remainder = temp % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            temp = temp /10;
         }
+        if (number == reversedNumber)
+            System.out.println("The number is Palindrome");
+        else
+            System.out.println("The number is not Palindrome");
     }
 }
